e7f9a6ebb750489a504f721f5eb8bedef882af7d
vladislavglad <vladglad.996@gmail.com>
2021-05-23 19:03:08 -0400

    Using appropriate objects to process the request.

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
 const express = require("express");
 const open = require("open");
 const app = express();
+
 const PORT = 3000;
+const URL = `http://127.0.0.1:${PORT}`;
 
 const Breakfast = require("./classes/breakfast");
+const Lunch = require("./classes/lunch");
+const Dinner = require("./classes/dinner");
 
 app.get("/", (req, res) => {
-    res.send("Place your order");
-})
+    res.send(`Place your order with something like: ${URL}/breakfast/1,2,3`);
+});
+
+app.get("/breakfast", (req, res) => {
+    res.send(`Provide arguments such as ${URL}/breakfast/1,2,3`);
+});
 
 app.get("/breakfast/:order", (req, res) => {
-    /* Note: "order" is of type string.
-    Plan: design a class that accepts a string
-    that represents an order and then validates it. */
     let breakfastOrder = new Breakfast(req.params.order);
     console.log(`got breakfast order: ${breakfastOrder}`);
     res.send(breakfastOrder.toString());
 });
 
+app.get("/lunch", (req, res) => {
+    res.send(`Provide arguments such as ${URL}/lunch/1,2,3`);
+});
+
 app.get("/lunch/:order", (req, res) => {
-    const {order} = req.params;
-    console.log(`got lunch order: ${order}`);
-    res.send(`here is your order: ${order}`);
+    let lunchOrder = new Lunch(req.params.order);
+    console.log(`got lunch order: ${lunchOrder}`);
+    res.send(lunchOrder.toString());
+});
+
+app.get("/dinner", (req, res) => {
+    res.send(`Provide arguments such as ${URL}/dinner/1,2,3,4`);
 });
 
 app.get("/dinner/:order", (req, res) => {
-    const {order} = req.params;
-    console.log(`got dinner order: ${order}`);
-    res.send(`here is your order: ${order}`);
+    let dinnerOrder = new Dinner(req.params.order);
+    console.log(`got dinner order: ${dinnerOrder}`);
+    res.send(dinnerOrder.toString());
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}/`);
+    console.log(`Server is running on ${URL}`);
 
     //opens the url in the default browser 
-    open("http://127.0.0.1:3000/");
+    open(URL);
 });
