2131f4bb232c0b26697474e1df232072a24a110d
vladislavglad <vladglad.996@gmail.com>
2021-05-23 11:06:00 -0400

    Getting basic input from the routes.

--- server.js
+++ server.js
@@ -6,16 +6,25 @@ app.get("/", (req, res) => {
     res.send("Place your order");
 })
 
-app.get("/breakfast", (req, res) => {
-    res.send("order breakfast")
+app.get("/breakfast/:order", (req, res) => {
+    /* Note: "order" is of type string.
+    Plan: design a class that accepts a string
+    that represents an order and then validates it. */
+    const {order} = req.params;
+    console.log(`got breakfast order: ${order}`);
+    res.send(`here is your order: ${order}`);
 });
 
-app.get("/lunch", (req, res) => {
-    res.send("order lunch")
+app.get("/lunch/:order", (req, res) => {
+    const {order} = req.params;
+    console.log(`got lunch order: ${order}`);
+    res.send(`here is your order: ${order}`);
 });
 
-app.get("/dinner", (req, res) => {
-    res.send("order dinner")
+app.get("/dinner/:order", (req, res) => {
+    const {order} = req.params;
+    console.log(`got dinner order: ${order}`);
+    res.send(`here is your order: ${order}`);
 });
 
 app.listen(port, () => console.log("Server is running!"));
