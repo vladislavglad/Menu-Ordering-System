26f89e83bd4c9a3cd64e30ea3709874a77a6348c
vladislavglad <vladglad.996@gmail.com>
2021-05-23 11:39:03 -0400

    Objects and decoding menu item from encoded strings.

---
+++ classes/breakfast.js
@@ -0,0 +1,34 @@
+class Breakfast {
+
+    static ITEM_ONE = "Eggs";
+    static ITEM_TWO = "Toast";
+    static ITEM_THREE = "Coffee";
+
+    constructor(order) {
+        this.order = [];
+
+        for (const itemCode of order) {
+            this.order.push(this.decodeItem(itemCode));
+        }
+    }
+
+    decodeItem(itemCode) {
+        switch(itemCode) {
+            case "1":
+                return Breakfast.ITEM_ONE;
+            case "2":
+                return Breakfast.ITEM_TWO;
+            case "3":
+                return Breakfast.ITEM_THREE;
+            default:
+                return "Undefined";
+        }
+    }
+
+    toString() {
+        return JSON.stringify(this.order);
+    }
+}
+
+let b = new Breakfast("123");
+console.log(b);
